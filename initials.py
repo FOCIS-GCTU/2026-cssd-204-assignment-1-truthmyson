da8b8d1f151777b9573784a36abc6a7fb67fa236
NAGETEY CHRISTOPHER <135829504+truthmyson@users.noreply.github.com>
2026-05-30 15:55:48 +0000

    Implement initials printing in stylized format
    Added a function to print initials in stylized large letters.

--- initials.py
+++ initials.py
@@ -1,6 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Christopher Nagetey
+# STUDENT ID: 2425401676
+# Email: 2425401676@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Christopher Nagetey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Printing my name as initials in large letters.
+    print()
+    print("CKN...")
+    print()
+
+    print("........CCCCCCC........KKK......KKK........NNNN.....NNN.....")
+    print("......CCCCCCCCC........KKK.....KKK.........NNNN.....NNN.....")
+    print(".....CCCC..............KKK....KKK..........NNNNN....NNN.....")
+    print("....CCC................KKK...KKK...........NNN.NN...NNN.....")
+    print("...CC..................KKKKKKKK............NNN..NN..NNN.....")
+    print("...CC..................KKKKKKKK............NNN..NN..NNN.....")
+    print("....CCC................KKK...KKK...........NNN...NN.NNN.....")
+    print(".....CCCC..............KKK....KKK..........NNN....NNNNN.....")
+    print("......CCCCCCCCC...**...KKK.....KKK....**...NNN.....NNNN...**")
+    print("........CCCCCCC...**...KKK......KKK...**...NNN.....NNNN...**")
+
+    print()
 
 
 
